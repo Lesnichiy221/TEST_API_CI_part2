6bc43b905411f00026b6046a837ca5965b6d2f0f
Vlad Test <vladkretovsky@yandex.ru>
2024-09-02 20:44:19 +0300

    added some tests, test shouldnt work

---
+++ src/test/java/ru/netology/test/PostmanEchoTestV1.java
@@ -0,0 +1,47 @@
+package ru.netology.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+class postmanEcho {
+
+    // Given - When - Then
+// Предусловия
+    @Test
+    void TestNumber1(){
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data ljshdfk"))
+        ;
+
+    }
+
+    @Test
+    void TestNumber2(){
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("какой - то текст") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("какой - то текст"))
+        ;
+
+    }
+}
